c9e315e06ddb8eacefdf9b1b2027b62f36126f2f
3amry <engahmedelamry@gmail.com>
2022-03-17 11:30:43 +0200

    Install angular firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from './../environments/environment';
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBPFBtq-AObejdft4et9oufUg8RNew4pbY',
+    authDomain: 'clips-88bf2.firebaseapp.com',
+    projectId: 'clips-88bf2',
+    storageBucket: 'clips-88bf2.appspot.com',
+    messagingSenderId: '412667584278',
+    appId: '1:412667584278:web:a4b6c0eb88fa923d407a37',
+  },
 };
