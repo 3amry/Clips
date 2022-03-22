0d11bd415ac2a3d172ad8730d532508853f70240
3amry <engahmedelamry@gmail.com>
2022-03-22 13:48:19 +0200

    Update clip with edited data.

--- src/app/video/edit-modal/edit-modal.component.ts
+++ src/app/video/edit-modal/edit-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -10,6 +11,10 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class EditModalComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null;
+  inSubmission = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Updating clip.';
 
   clipID = new FormControl('');
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);
@@ -19,7 +24,7 @@ export class EditModalComponent implements OnInit, OnDestroy, OnChanges {
     id: this.clipID,
   });
 
-  constructor(private modal: ModalService) {}
+  constructor(private modal: ModalService, private clipService: ClipService) {}
 
   ngOnInit(): void {
     this.modal.register('editClip');
@@ -34,5 +39,25 @@ export class EditModalComponent implements OnInit, OnDestroy, OnChanges {
 
     this.clipID.setValue(this.activeClip.docID);
     this.title.setValue(this.activeClip.title);
+  }
+
+  async submit() {
+    this.inSubmission = true;
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Updating clip.';
+
+    try {
+      await this.clipService.updateClip(this.clipID.value, this.title.value);
+    } catch (error) {
+      this.inSubmission = false;
+      this.alertColor = 'red';
+      this.alertMsg = 'Something went wrong. Try again later';
+      return;
+    }
+
+    this.inSubmission = false;
+    this.alertColor = 'green';
+    this.alertMsg = 'Success!';
   }
 }
