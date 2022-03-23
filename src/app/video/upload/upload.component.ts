2c0d3eeca9d1c610ba10d3eb4b73e9a74be330d8
3amry <engahmedelamry@gmail.com>
2022-03-23 05:01:00 +0200

    Upload screenshot blob to firebase.

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -31,6 +31,7 @@ export class UploadComponent implements OnDestroy {
   task?: AngularFireUploadTask;
   screenshots: string[] = [];
   selectedScreenshot = '';
+  screenshotTask?: AngularFireUploadTask;
 
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
@@ -74,7 +75,7 @@ export class UploadComponent implements OnDestroy {
     this.title.setValue(this.file?.name.replace(/\.[^/.]+$/, ''));
   }
 
-  uploadFile() {
+  async uploadFile() {
     this.uploadForm.disable();
     this.showAlert = true;
     this.alertColor = 'blue';
@@ -85,8 +86,15 @@ export class UploadComponent implements OnDestroy {
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
+    const screenshotBlob = await this.ffmpegService.blobFromURL(
+      this.selectedScreenshot
+    );
+    const screenshotPath = `screenshots/${clipFileName}.png`;
+
     this.task = this.storage.upload(clipPath, this.file);
     const clipRef = this.storage.ref(clipPath);
+
+    this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob);
 
     this.task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
