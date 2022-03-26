99863c8f27b698bf7762ab6d2d3f425b0e4ed0bd
3amry <engahmedelamry@gmail.com>
2022-03-26 09:49:34 +0200

    Fix video styles & disable angular css encapsulation in clip component.

--- src/app/components/clip/clip.component.ts
+++ src/app/components/clip/clip.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
 
@@ -6,6 +12,7 @@ import videojs from 'video.js';
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ClipComponent implements OnInit {
   id = '';
